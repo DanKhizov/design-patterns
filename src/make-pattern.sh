6f33f83e2db620897cb82337848efcbad9f2ab22
DanKhizov <dan.khizov@gmail.com>
2019-11-05 00:39:08 +0300

    Updated pattern creation script

--- src/make-pattern.sh
+++ src/make-pattern.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
-# Ask the user for their name
-echo Enter pattern name
-read patternName
 
-mkdir src/$patternName
-touch src/$patternName/$patternName.md
-touch src/$patternName/$patternName.ts
-touch src/$patternName/$patternName.test.ts
+echo Enter pattern category by number:
+echo 1 - Creational
+echo 2 - Structural
+echo 3 - Test
+
+read PATTERN_CATEGORY_NUM
+
+case $PATTERN_CATEGORY_NUM in
+  1)
+    PATTERN_DIR="CreationalPatterns"
+    ;;
+
+  2)
+    PATTERN_DIR="StructuralPatterns"
+    ;;
+
+  3)
+    PATTERN_DIR="BehavioralPatterns"
+    ;;
+esac
+
+echo Enter pattern name
+read PATTERN_NAME
+
+mkdir src/$PATTERN_DIR
+touch src/$PATTERN_DIR/$PATTERN_NAME.md
+touch src/$PATTERN_DIR/$PATTERN_NAME.ts
+touch src/$PATTERN_DIR/$PATTERN_NAME.test.ts
